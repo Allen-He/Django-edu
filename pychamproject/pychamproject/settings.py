3d8f704bd8d1114ad68ea2d5c7542897c23702e6
Allen-He <494189626@qq.com>
2017-09-19 21:43:05 +0800

    mysql-python

--- pychamproject/pychamproject/settings.py
+++ pychamproject/pychamproject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'testdjango',
         'USER':'root',
         'PASSWORD':'root',
-        'HOST':'127.0.0.1'),
+        'HOST':'127.0.0.1'
     }
 }
 
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
