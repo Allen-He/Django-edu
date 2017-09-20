7b83224b4ea14afb2549e9a8e9de2b0bf508e9d0
Allen-He <494189626@qq.com>
2017-09-20 21:47:36 +0800

    models

--- pychamproject/apps/message/views.py
+++ pychamproject/apps/message/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import MySQLdb
 
 # Create your views here.
 
 def getform(request):
     return render(request,'message_index.html')
+
+# def book_list(request):
+#
