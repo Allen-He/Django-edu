5960aa581a86a82d083ca7efa6351c9fdf9d480d
Allen-He <494189626@qq.com>
2017-10-17 21:33:55 +0800

    user models

--- MxOnline/users/models.py
+++ MxOnline/users/models.py
@@ -1,3 +1,4 @@
+# _*_encoding:utf-8 _*_
 from __future__ import unicode_literals
 
 from django.db import models
@@ -5,7 +6,19 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=r"昵称", default=" ")
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
-    gender = models.CharField(choices=(("male",u"男"),("female",u"女")),default="female")
+    gender = models.CharField(max_length=5,choices=(("male",u"男"),("female",u"女")),default="female")
+    address = models.CharField(max_length=100,default=u"")
+    mobile = models.CharField(max_length=11,null=True,blank=True)
+    image = models.ImageField(upload_to="image/%Y/%m",default=u"image/default.png",max_length=100)
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+        def __unicode__(self):
+            return self.username
+            #重写Unicode方法，才可输出自定义字符串
